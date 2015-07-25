d60e4e4d141cc4dd9530641c46bd1d6c0d4faa5e
Richard Fussenegger <fleshgrinder@gmx.at>
2015-07-25 09:00:37 +0200

    Fixed project name in license block.

--- optics.js
+++ optics.js
@@ -1,16 +1,16 @@
 /*!
- * This file is part of fleshgrinder/fhs-masters-thesis-benchmarks.
+ * This file is part of Fleshgrinder/OPTICS-algorithm.
  *
- * fleshgrinder/fhs-masters-thesis-benchmarks is free software: you can redistribute it and/or modify it under the terms
- * of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * Fleshgrinder/OPTICS-algorithm is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
  *
- * fleshgrinder/fhs-masters-thesis-benchmarks is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Affero General Public License for more details.
+ * Fleshgrinder/OPTICS-algorithm is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License along with fleshgrinder/fhs-masters-thesis-
- * benchmarks. If not, see <https://www.gnu.org/licenses/agpl-3.0.html>.
+ * You should have received a copy of the GNU Affero General Public License along with Fleshgrinder/OPTICS-algorithm.
+ * If not, see <https://www.gnu.org/licenses/agpl-3.0.html>.
  */
 
 /**
